01e58858e9b59157bd650c4001e7fff83d5cf1a8
ozca24tl <ozca24tl@gmail.com>
2026-06-24 00:54:19 +0200

    js

--- tim.js
+++ tim.js
@@ -7,8 +7,20 @@ console.log(`selam=${A}`)
 console.error("error");// for red error
 console.warn("warning");//yellowish warning 
 
-
+////////////////////////////////////////////////
 const prompt = require("prompt-sync")();
 
 const v = prompt("type something");
 console.log(v);
+
+////////////////////////////////////////////////
+
+const x = "234.5";
+const y = 2;
+const z = 2323;
+
+
+console.log(Number(x) + y); //turns the string into number 
+console.log(parseInt(x) + y);//" " " " "
+console.log(string(x) + y);
+
