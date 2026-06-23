d52229985f8b3f91d05483d3cd2ab3b29545ac85
ozca24tl <ozca24tl@gmail.com>
2026-06-23 16:45:25 +0200

    npm

---
+++ tim.js
@@ -0,0 +1,8 @@
+//in terminal use "node tim.js" to see outcome faster//
+
+const A = "sa";
+
+console.log("sa"); // for message 
+console.log(`selam=${A}`)
+console.error("error");// for red error
+console.warn("warning");//yellowish warning 
