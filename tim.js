af313919a878a3251c2e6a7f7b430c9873882cd5
ozca24tl <ozca24tl@gmail.com>
2026-06-23 22:46:01 +0200

    js

--- tim.js
+++ tim.js
@@ -6,3 +6,9 @@ console.log("sa"); // for message
 console.log(`selam=${A}`)
 console.error("error");// for red error
 console.warn("warning");//yellowish warning 
+
+
+const prompt = require("prompt-sync")();
+
+const v = prompt("type something");
+console.log(v);
